4ae2b59a777013250c41d6598e3ab744e66c6584
Kennedy Kori <kennedykori47@gmail.com>
2022-07-06 17:03:26 +0300

    feat: Implement config loading bootstrap

--- tests/lib/test_module_loading.py
+++ tests/lib/test_module_loading.py
@@ -1,9 +1,10 @@
 import pytest
 
-from app.lib import Config, import_string
+from app.core import Task
+from app.lib import Config, Consumer, import_string, import_string_as_klass
 
 
-def test_correct_return_value_on_valid_dotted_path() -> None:
+def test_correct_return_value_on_valid_dotted_path1() -> None:
     """
     Assert that when given a valid dotted path as input, ``import_string``
     returns the correct class or attribute.
@@ -14,7 +15,7 @@ def test_correct_return_value_on_valid_dotted_path() -> None:
     )  # noqa
 
 
-def test_correct_expected_behavior_on_invalid_dotted_path() -> None:
+def test_correct_expected_behavior_on_invalid_dotted_path1() -> None:
     """
     Assert that an ``ImportError`` is raised when an invalid dotted path is
     given as input to ``import_string``.
@@ -23,3 +24,28 @@ def test_correct_expected_behavior_on_invalid_dotted_path() -> None:
         import_string("aninvalidpath")
     with pytest.raises(ImportError):
         import_string("app.no_such")
+
+
+def test_correct_return_value_on_valid_dotted_path2() -> None:
+    """
+    Assert that when given a valid dotted path as input,
+    ``import_string_import_string_as_klass`` returns the correct class.
+    """
+    assert import_string_as_klass("app.lib.Config", Config) is Config
+    assert import_string_as_klass("app.lib.Consumer", Task) is Consumer
+
+
+def test_correct_expected_behavior_on_invalid_dotted_path2() -> None:
+    """
+    Assert that an ``ImportError`` or ``TypeError`` is raised when an invalid
+    dotted path is given as input to ``import_import_string_as_klass``.
+    """
+    with pytest.raises(ImportError):
+        import_string_as_klass("aninvalidpath", Config)
+    with pytest.raises(TypeError):
+        import_string_as_klass(
+            "app.lib.module_loading.import_string",
+            import_string,  # type: ignore
+        )
+    with pytest.raises(TypeError):
+        import_string_as_klass("app.lib.Config", Task)
